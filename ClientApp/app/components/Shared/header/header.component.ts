9123747426ee11010044215f83c4a2bef58620c6
Katherine <katherinematthews.polsci@gmail.com>
2017-05-15 12:22:57 -0700

    starting from template, create header component that displays on all pages.

---
+++ ClientApp/app/components/Shared/header/header.component.ts
@@ -0,0 +1,21 @@
+﻿import { Component, Input, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { RequestOptions, Request, RequestMethod } from '@angular/http';
+
+
+@Component({
+    selector: 'shared-header',
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent implements OnInit {
+    public user: string;
+
+    ngOnInit() {
+        this.user = "Rachel";
+    }
+
+    constructor() {
+    }
+
+
+}
